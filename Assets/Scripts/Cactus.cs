bf4546cb9d0534e1d1786a519fc8d366ebe3bf32
unknown <beatrizvazpinto@hotmail.com>
2020-07-27 18:38:01 -0300

    initial commit

---
+++ Assets/Scripts/Cactus.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cactus : MonoBehaviour {
+
+    public GameObject player;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        checkIfFallen();
+        //var collider = gameObject.GetComponent<BoxCollider2D>();
+        //if (collider.gameObject == player)
+        //{
+        //    gameObject.GetComponent<Animator>().SetBool("falls", true);
+        //}
+	}
+
+    void OnTriggerEnter2D(Collider2D collider)
+    {
+        if (collider.gameObject.tag == "Player")
+        {
+            gameObject.GetComponent<Animator>().SetBool("falls", true);
+            Debug.Log("start");
+        }
+    }
+
+    void checkIfFallen()
+    {
+        if (gameObject.transform.position.y < -3.2)
+        {
+           gameObject.transform.position = new Vector3(gameObject.transform.position.x, -5, gameObject.transform.position.z);
+           gameObject.GetComponent<Animator>().SetBool("explodes", true);
+           // Destroy(gameObject);
+        }
+
+
+    }
+}
---
+++ Assets/Scripts/PlayerControls.cs
@@ -0,0 +1,84 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PlayerControls : MonoBehaviour {
+
+    public int playerSpeed = 10;
+    public int playerJumpPower;
+    public bool isGrounded;
+    private bool facingRight = true;
+    private float moveX;
+    private Animator anim;
+    public int health;
+    public bool hasDied;
+
+    void Start() {
+        hasDied = false;
+        anim = gameObject.GetComponent<Animator>();
+    }
+
+	void Update() {
+        MovePlayer();
+        CheckIfDead();
+	}
+
+    void OnCollisionEnter2D(Collision2D collider) {
+        if (collider.gameObject.tag == "Ground" || collider.gameObject.tag == "Platform")
+        {
+            isGrounded = true;
+        }
+    }
+
+    void CheckIfDead() {
+        if (gameObject.transform.position.y < -5) {
+            Debug.Log("Oh no!");
+            hasDied = true;
+        }
+        if (hasDied) {
+            // SceneManager.LoadScene("CatScene");
+            gameObject.transform.position = new Vector3(-2, -2, gameObject.transform.position.z);
+            hasDied = false;
+        }
+
+    }
+
+    void MovePlayer() {
+        moveX = Input.GetAxis("Horizontal");
+        if (Input.GetButtonDown("Jump") && isGrounded) {
+            Jump();
+        }
+        if (Input.GetButtonDown("Horizontal")) {
+            anim.SetInteger("state", 1);
+        }
+        if (Input.GetButtonUp("Horizontal")) {
+            anim.SetInteger("state", 0);
+        }
+        if (moveX < 0.0f && facingRight) {
+            FlipPlayer();
+        }
+        else if (moveX > 0.0f && !facingRight) {
+            
+            FlipPlayer();
+        }
+
+        gameObject.GetComponent<Rigidbody2D>().velocity = new Vector2(moveX * playerSpeed, gameObject.GetComponent<Rigidbody2D>().velocity.y);
+    }
+
+    void Jump() {
+        gameObject.GetComponent<Rigidbody2D>().AddForce(Vector2.up * playerJumpPower);
+        isGrounded = false;
+    }
+
+    void FlipPlayer() {
+        facingRight = !facingRight;
+        var localScale = gameObject.transform.localScale;
+        localScale.x *= -1;
+        transform.localScale = localScale;
+    }
+}
+
+//https://opengameart.org/content/24x32-characters-with-faces-big-pack
+//Svetlana Kushnariova and give my email address, lana-chan@yandex.ru
+//http://neoriceisgood.deviantart.com/art/100-furniture-sprites-405058884
